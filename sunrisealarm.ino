893333a23e2f00783f39fa19ac6f6aa8898eb159
Tobi <tobias.eichner@gmail.com>
2019-01-10 22:15:16 +0100

    added cloud variable for DST

--- sunrisealarm.ino
+++ sunrisealarm.ino
@@ -15,6 +15,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
 // alarm enabled status
 bool isAlarmEnabled = true;
+bool isDstOn = false;
 
 // define alarm id arrays
 int weekdayAlarmId[] = { dtINVALID_ALARM_ID, dtINVALID_ALARM_ID, dtINVALID_ALARM_ID, dtINVALID_ALARM_ID, dtINVALID_ALARM_ID};
@@ -25,8 +26,8 @@ timeDayOfWeek_t weekendDays[] = { dowSaturday, dowSunday };
 // alarm variables
 unsigned long lastAlarmStarted = -1;
 unsigned int offMin = 60;
-String weekdayStatus = "<not set>";
-String weekendStatus = "<not set>";
+String weekdayStatus = "<n/a>";
+String weekendStatus = "<n/a>";
 
 // define sunrise color gradient
 unsigned int redList[] = { 0,8,17,26,35,43,52,61,70,79,87,96,105,114,123,131,140,149,158,167,175,184,193,202,211,219,228,237,246,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,254,253,253,252,252,251,250,250,249,249,248,247,247,246,246,245,245,244,243,243,242,242,241,240,240,239,239,238,237,237,236,236,235,235,234,233,233,232,232,231,230,230,229,229,228,228,227,226,226,225,225,224,223,223,222,222,221,220,220,219,219,218,218,217,216,216,215,215,214,213,213,212,212,211,211,210,209,209,208,208,207,206,206,205,205,204,203,203,202,202,201,201,200,199,199,198,198,197,196,196,195,195,194,193,193,192,192,191,191,190,189,189,188,188,187,186,186,185,185,184,184,183,182,182,181,181,180,179,179,178,178,177,176,176,175,175,174,174,173,172,172,171,171,170,169,169,168,168,167,167,166,165,165,164,164,163,162,162,161,161,160,159,159,158,158,157,157,156,155,155,154,154,153,152,152,151,151,150,150,149,148,147,146,145,144,142,141,140,139,138,137,136,135,134,133,132,131,130,129,128 };
@@ -82,6 +83,7 @@ void setup() {
     
     // register cloud variables
     Particle.variable("alarmEnabled", isAlarmEnabled);
+    Particle.variable("dstEnabled", isDstOn);
     Particle.variable("wdStatus", weekdayStatus);
     Particle.variable("weStatus", weekendStatus);
     
@@ -137,9 +139,11 @@ int cloudAlarmOn(String command) {
 int cloudDstOn(String command) {
     if (command == "1") {
         Time.zone(+2);
+        isDstOn = true;
         return 1;
     } else {
         Time.zone(+1);
+        isDstOn = false;
         return 0;
     }
 }
