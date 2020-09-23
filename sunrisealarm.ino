58117c146d7087f84bede5e583c38569d5941e41
tbmsu <hallo@tbmsu.com>
2020-09-23 21:29:36 +0200

    fixed string-to-int conversion
    also added debug variable to check string-to-int conversion

--- sunrisealarm.ino
+++ sunrisealarm.ino
@@ -1,12 +1,12 @@
 // This #include statement was automatically added by the Particle IDE.
-#include "TimeAlarms.h"
-
-// This #include statement was automatically added by the Particle IDE.
 #include <neopixel.h>
 
+// This #include statement was automatically added by the Particle IDE.
+#include "TimeAlarms.h"
+
 #define PIXEL_PIN D2
-#define PIXEL_TYPE WS2812B
-#define PIXEL_COUNT 233
+#define PIXEL_TYPE WS2812B_FAST
+#define PIXEL_COUNT 60 // 232
 
 #define ONE_DAY_MILLIS (24 * 60 * 60 * 1000)
 unsigned long lastSync = millis();
@@ -31,6 +31,7 @@ unsigned long lastAlarmStarted = -1;
 String weekdayStatus = "{}";
 String weekendStatus = "{}";
 String globalStatus = "{}";
+String debug = String("n/a");
 
 // define sunrise color gradient
 unsigned int redList[] = { 0,8,17,26,35,43,52,61,70,79,87,96,105,114,123,131,140,149,158,167,175,184,193,202,211,219,228,237,246,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,254,253,253,252,252,251,250,250,249,249,248,247,247,246,246,245,245,244,243,243,242,242,241,240,240,239,239,238,237,237,236,236,235,235,234,233,233,232,232,231,230,230,229,229,228,228,227,226,226,225,225,224,223,223,222,222,221,220,220,219,219,218,218,217,216,216,215,215,214,213,213,212,212,211,211,210,209,209,208,208,207,206,206,205,205,204,203,203,202,202,201,201,200,199,199,198,198,197,196,196,195,195,194,193,193,192,192,191,191,190,189,189,188,188,187,186,186,185,185,184,184,183,182,182,181,181,180,179,179,178,178,177,176,176,175,175,174,174,173,172,172,171,171,170,169,169,168,168,167,167,166,165,165,164,164,163,162,162,161,161,160,159,159,158,158,157,157,156,155,155,154,154,153,152,152,151,151,150,150,149,148,147,146,145,144,142,141,140,139,138,137,136,135,134,133,132,131,130,129,128 };
@@ -38,14 +39,13 @@ unsigned int greenList[] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 unsigned int blueList[] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,5,6,7,9,10,11,12,14,15,16,18,19,20,22,23,24,25,27,28,29,31,32,33,34,36,37,38,40,41,42,44,45,46,47,49,50,51,53,54,55,57,58,59,60,62,63,64,66,67,68,69,71,72,73,75,76,77,79,80,81,82,84,85,86,88,89,90,92,93,94,95,97,98,99,101,102,103,104,106,107,108,110,111,112,114,115,116,117,119,120,121,123,124,125,127,128,129,130,132,133,134,136,137,138,139,141,142,143,145,146,147,149,150,151,152,154,155,156,158,159,160,162,163,164,165,167,168,169,171,172,173,174,176,177,178,180,181,182,184,185,186,187,189,190,191,193,194,195,197,198,199,200,202,203,204,206,207,208,209,211,212,213,215,216,217,219,220,221,222,224,225,226,228,229,230,232,233,234,235,236,237,238,240,241,242,243,244,245,246,247,248,249,250,251,252,253,255 };
 
 // define number of color values {0..300}
-unsigned int colorValues = 210;
+unsigned int colorValues = 210; // 210
 
 // define sunrise duration (times #colorValues)
-unsigned int duration = 8600;
+unsigned int duration = 4300; // 8600
 
 // calculated sunrise duration in milliseconds
 unsigned int sunriseDurationMs = colorValues * duration;
-
 
 void color(uint8_t r, uint8_t g, uint8_t b, int leds) {
     uint16_t pixel;
@@ -59,7 +59,7 @@ void color(uint8_t r, uint8_t g, uint8_t b, int leds) {
 
 
 void pixelsOff() {
-    color(0, 0, 0, PIXEL_COUNT);
+    color(0, 0, 0, strip.numPixels());
 }
 
 
@@ -88,13 +88,15 @@ void setup() {
     Particle.function("dstOn", cloudDstOn);
     Particle.function("wdCmd", cloudHandleWeekdayParams);
     Particle.function("weCmd", cloudHandleWeekendParams);
+    Particle.function("off", cloudOff);
     
     // register cloud variables
     Particle.variable("status", globalStatus);
+    Particle.variable("debug", debug);
     
     // init strip
     strip.begin();
-    strip.setBrightness(96);
+    strip.setBrightness(64); // 96
     strip.show();
     pixelsOff();
 }
@@ -131,6 +133,11 @@ void MorningOffAlarm() {
 
 
 //======================== CLOUD FUNCTIONS ===========================
+int cloudOff(String command) {
+    pixelsOff();
+    return 1;
+}
+
 int cloudAlarmOn(String command) {
     int result;
     if (command == "1") {
@@ -166,24 +173,39 @@ int cloudDstOn(String command) {
 int cloudHandleWeekdayParams(String command) {
     if (command.length() != 9) return -1;
     
+    debug = String("");
+    debug.concat(command);
+    debug.concat(": ");
+    
     // get command components
-    String days = command.substring(0, 5);
-    String hour = command.substring(5, 7);
-    String min  = command.substring(7);
+    String days = String(command.substring(0, 5));
+    String hour = String(command.substring(5, 7));
+    String min  = String(command.substring(7));
     
-    int i_hour, i_min;
-    
-    if ('0' == hour.charAt(0)) { 
-        i_hour = hour.substring(1).toInt();
+    String s_hour, s_min;
+    long i_hour, i_min;
+
+    if (hour.startsWith("0")) {
+        s_hour = hour.substring(1,2);
     } else {
-        i_hour = hour.toInt();
+        s_hour = hour;
     }
+    char charBuf_hour[2];
+    s_hour.toCharArray(charBuf_hour, 2);
+    i_hour = atoi(charBuf_hour);
     
-    if ('0' == min.charAt(0)) { 
-        i_min = min.substring(1).toInt();
+    if (min.startsWith("0")) { 
+        s_min = min.substring(1,2);
     } else {
-        i_min = min.toInt();
+        s_min = min;
     }
+    char charBuf_min[2];
+    s_min.toCharArray(charBuf_min, 2);
+    i_min = atoi(charBuf_min);
+    
+    debug.concat(String(s_hour));
+    debug.concat(String(':'));
+    debug.concat(String(s_min));
     
     // free alarms
     for (unsigned int d = 0; d < sizeof(weekdayAlarmId); d++) {
@@ -196,7 +218,7 @@ int cloudHandleWeekdayParams(String command) {
     // register daily alarms (weekdays)
     for (unsigned int d = 0; d < sizeof(days); d++) {
         if (days.charAt(d) == '1') {
-            // set alarm ans store alarm ID
+            // set alarm and store alarm ID
             weekdayAlarmId[d] = Alarm.alarmRepeat(weekdayDays[d], i_hour, i_min, 0, MorningAlarm);
         } else {
             // set invalid alarm ID for day
@@ -215,24 +237,39 @@ int cloudHandleWeekdayParams(String command) {
 int cloudHandleWeekendParams(String command) {
     if (command.length() != 6) return -1;
     
+    debug = String("");
+    debug.concat(command);
+    debug.concat(": ");
+    
     // get command components
     String days = command.substring(0, 2);
     String hour = command.substring(2, 4);
     String min  = command.substring(4);
     
-    int i_hour, i_min;
-    
-    if ('0' == hour.charAt(0)) { 
-        i_hour = hour.substring(1).toInt();
+    String s_hour, s_min;
+    long i_hour, i_min;
+
+    if (hour.startsWith("0")) {
+        s_hour = hour.substring(1,2);
     } else {
-        i_hour = hour.toInt();
+        s_hour = hour;
     }
+    char charBuf_hour[2];
+    s_hour.toCharArray(charBuf_hour, 2);
+    i_hour = atoi(charBuf_hour);
     
-    if ('0' == min.charAt(0)) { 
-        i_min = min.substring(1).toInt();
+    if (min.startsWith("0")) { 
+        s_min = min.substring(1,2);
     } else {
-        i_min = min.toInt();
+        s_min = min;
     }
+    char charBuf_min[2];
+    s_min.toCharArray(charBuf_min, 2);
+    i_min = atoi(charBuf_min);
+    
+    debug.concat(String(s_hour));
+    debug.concat(String(':'));
+    debug.concat(String(s_min));
     
     // free alarms
     for (unsigned int d = 0; d < sizeof(weekendAlarmId); d++) {
@@ -245,7 +282,7 @@ int cloudHandleWeekendParams(String command) {
     // register daily alarms (weekend)
     for (unsigned int d = 0; d < sizeof(days); d++) {
         if (days.charAt(d) == '1') {
-            // set alarm ans store alarm ID
+            // set alarm and store alarm ID
             weekendAlarmId[d] = Alarm.alarmRepeat(weekendDays[d], i_hour, i_min, 0, MorningAlarm);
         } else {
             // set invalid alarm ID for day
